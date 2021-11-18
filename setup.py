9d9972e32954825a47dd4e3769087e8936198b90
Michael Gordon <m.gordon@fz-juelich.de>
2021-11-18 09:43:12 +0100

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,37 +3,45 @@ import os
 import stat
 
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 
 def find_executable(path):
-    executable = stat.S_IEXEC | stat.S_IXGRP | \
-                 stat.S_IXOTH | (not stat.S_ISLNK)
+    executable = stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH | (not stat.S_ISLNK)
 
     res = []
     for dp, dn, filenames in os.walk(path):
-        if '.git/' in dp:
+        if ".git/" in dp:
             continue
 
         for f in filenames:
-            if not (os.stat(os.path.join(dp,f)).st_mode & executable):
+            if not (os.stat(os.path.join(dp, f)).st_mode & executable):
                 continue
 
-            res += [os.path.join(dp,f)]
+            res += [os.path.join(dp, f)]
 
     return res
 
 
 setuptools.setup(
-    name='cassandra_io',
-    version='0.1',
-    author='Evgenii Sovetkin',
-    author_email='e.sovetkin@gmail.com',
-    description='Few tools to store files in cassandra',
+    name="cassandra_io",
+    version="0.1",
+    author="Evgenii Sovetkin",
+    author_email="e.sovetkin@gmail.com",
+    description="Few tools to store files in cassandra",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='git@github.com:esovetkin/open-elevation',
+    url="git@github.com:esovetkin/open-elevation",
     packages=setuptools.find_packages(),
-    scripts=find_executable('scripts/')
+    scripts=find_executable("scripts/"),
+    install_requires=[
+        "cassandra-driver",
+        "python-geohash",
+        "numpy",
+        "tqdm",
+        "shapely",
+        "rtree",
+        "pytest",
+    ],
 )
